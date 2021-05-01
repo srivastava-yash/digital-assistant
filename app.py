6d63f77ee55f020510471f619eeee246cd6b94fc
Yash Srivastava <yash.184040@gmail.com>
2021-05-01 21:09:52 +0300

    app.py updated

--- app.py
+++ app.py
@@ -7,14 +7,21 @@ app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Distant Assistant</h1><p>This is an API which classifies the intent for our digital assistant</p>"
+    return "<h1>Digital Assistant</h1><p>This is an API which classifies the intent for our digital assistant</p>", 200
 
-#sample url to call - http://127.0.0.1:5000/jarvis?input=give+me+the+details+of+shiv+dutt
+#sample url to call - http://127.0.0.1:5000/jarvis?str=give+me+the+details+of+shiv+dutt
 @app.route('/jarvis', methods=['GET'])
 def jarvis():
-    #yaha pe apna model wala function phir json ya jo bhi return mardenge
-    #return jsonify()
-    return "<h1>Json lelo</h1>"
+    args = request.args
+    str = ""
+    if "str" in args:
+        str = args.get("str")
+    if str != "":
+        input = str.split("+")
+        input = " ".join(input)
+    print(input)
+    #yaha pe apna model wala function call kardenge phir json ya jo bhi return mardenge
+    return "<h1>Json lelo</h1>", 200
 
 # server running on port 5000
 app.run()
